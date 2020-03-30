72b022673f40ada8df4a85cf7dccf3f14ebd9fd0
ArmandLng <armand.langle@edu.devinci.fr>
2020-03-31 00:07:41 +0200

    Initialisation

---
+++ Initialisation.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+class Plateau:
+
+    def __init__(self):
+        #Lien web utilisé : cours OpenclassRoom 
+        #https://openclassrooms.com/fr/courses/4452741-decouvrez-les-librairies-python-pour-la-data-science/4740941-plongez-en-detail-dans-la-librairie-numpy
+
+        """
+        Une liste de listes est transformée en un tableau multi-dimensionnel     
+        """
+        self.size_Ligne = 19
+        self.size_Colonne = 19
+        self.myMat = np.full((self.size_Ligne, self.size_Colonne), ".", dtype=str)
+        #print(type(myMat[0,0])) #Pour s'assurer du type des valeurs dans la matrice
+        """
+        np.full((size_Ligne, size_Colonne), ".", dtype=str)
+        *Crée un tableau 19x19 rempli du caratère "."
+        *dtype = str : On force le type de donnée en précisant que l'on veut un string
+        *np.full((n,p), 5) : Va créer un tableau multi-dimensionnel de dimension n lignes et p colonnes et va remplir chaque case par la valeur 5 qui par défaut est un int
+        """
+        #print(self.myMat)#Affichage brut de myMat
+
+    def __str__(self):
+        """
+        Affichage de la matrice = Grille de jeu       
+        #! SENS D'AFFICHAGE : Du haut vers le bas, gauche à droite => Matrice affichée dans le sens commun donc remplir d'abord le bas de la matrice
+        Infos données par les valeurs sur le côté de la matrice : indices de la matrice ligne (vertical) et colonne(horizontal)
+        *Lien Web:
+         * Actuel : https://stackoverflow.com/questions/17870612/printing-a-two-dimensional-array-in-python
+         * Autre possiblité : Tkinter : http://softdmi.free.fr/tuto_python/tuto-python.php?chapitre=2&page=2 
+        """        
+        for j in range(0,self.size_Colonne):
+            print(("  "+ str(j) + " ") if(j<10)else ("  "+ str(j) +""), end="") 
+            #Léger bricolage : Amélioration possible avec un autre affichage : Tkinter / GUI
+        print()
+        for i in range(0,self.size_Ligne):
+            for j in range(0,self.size_Colonne):
+                print("|", end = " ")
+                print (self.myMat[i,j], sep="",end=" ")
+            print("| " + str(i), end = " ")       
+            print("")
+        for j in range(0,self.size_Colonne*4+1):
+            print("-", end="")
+
+        return("\nVous pouvez jouer !")
+        
+
+if __name__ == '__main__':
+    mat = Plateau()
+    print(mat)
+
