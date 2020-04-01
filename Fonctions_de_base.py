5fe782848fb43b20d6c938f1410f9b6bd981f0ec
Clement Valot <val.croissy@gmail.com>
2020-04-01 19:42:23 +0200

    fonction action

--- Fonctions_de_base.py
+++ Fonctions_de_base.py
@@ -10,23 +10,20 @@ Liste les actions possibles à partir d'un état donné
 
 UNIT TEST FAIT
 '''
-#Cette méthode va retourner toutes les actions possibles à l'état state pour le joueur.
-#On va donc retourner une liste de 19 entiers : l'indice de la liste correspond au numéro de colonne et 
-#l'entier à cet indice correspond au numéro de ligne.
-#On va parcourir la matrice colonne par colonne. On part du haut de chaque colonne et tant qu'on n'est pas
-#tombé sur une case non vide, on continue à la ligne suivante. Lorsque l'on tombe sur une case non vide,
-#On rentre le numéro de la ligne de la case au dessus.
+
+'''
+Cette méthode va renvoyer la liste des colonnes dans lesquelles on peut encore jouer.
+On a donc besoin de juste parcourir la première ligne de la matrice : 
+   * si la case de la colonne est vide, alors on peut jouer dans cette colonne
+   * sinon, on ne peut pas jouer dedans car la colonne est remplie.
+'''
 def Action (state, joueur):
     liste_actions = []
-    for i in range(nb_Colonne):
-        for j in range (nb_Ligne):
-            if(state[j][i] == 'X' or state[j][i] == 'O'):
-                liste_actions.append(nb_Ligne-1)
-                break
-            #Si on a parcouru toutes les cases de la colonne et que toutes étaient vides, 
-            #on ajoute le numéro de cette dernière ligne à la liste
-            elif(j==nb_Ligne-1):
-                liste_action.append(j)
+    ligne=0
+    for j in range (nb_Colonne):
+        if(state[ligne,j] != 'X' and state[ligne,j] != 'O'):
+            liste_actions.append(j)
+            break
     return liste_actions
 
 '''
