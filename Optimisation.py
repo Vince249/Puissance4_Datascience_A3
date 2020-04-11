dc557f01dcb5f7caccf3bd7f4df0226dfdf90cd2
ArmandLng <armand.langle@edu.devinci.fr>
2020-04-11 13:11:12 +0200

    Facilite de lecture

--- Optimisation.py
+++ Optimisation.py
@@ -2,7 +2,6 @@ import Fonctions_de_base
 import Initialisation
 import time
 from shutil import get_terminal_size #pour fonction clear
-import random #Pour choisir les textes à afficher
 
 
 #!Section copiée/Collée de AlphaBetaMiniMax
@@ -180,28 +179,6 @@ def Selection_colonne(phrase):
             print("Erreur : type de l'input")
     return action
 
-#Partie du Lore car c'est inutile mais absolument indispensable
-'''Lore pour introduire le jeu
-'''
-def Intro():
-    print("Bienvenue à toi Champion !",
-    "\nSache que toute entrevue avec Athéna nécessite un Test.",
-    "\nProuve nous ton sens de la stratégie et fais honneur à notre Déesse !",
-    "\n\nTu devras affronter notre champion !!!"
-    "\n\nCelui-là même qui sera créé par ton espèce dans un futur lointain !",
-    "\nSi tu l'emportes face à Skynet, Athéna t'écoutera !\n\n")
-'''
-Reactions utilisées à chaque coups de l'adversaire
-'''
-def Reaction():
-    a = "Voilà un coup intéressant ! \nMais il en faudra plus pour me surprendre !"
-    b = "N'oublie pas ! La vitesse est une chose mais la précipitation en est une autre."
-    c = "Cela faisait plus d'un siècle que je ne m'étais pas amusé de la sorte !\nContinue de me divertir !"
-    d = "Hmmmm\nJe ne distingue plus ton futur ..."
-    e = "Le fin joueur ne joue pas la carte qu'attend son adversaire, encore moins celle qu'il désire"
-    f = "Connaître exactement le jeu de ses adversaires, c'est leur avoir déjà à moitié gagné la partie."
-    return (random.choice([a,b,c,d,e,f]))#Sélection aléatoire d'une citation
-
 
 #! SECTION MAIN
 
@@ -261,7 +238,7 @@ if __name__ == '__main__':
                 if(check_partie_fini) : break
 
         print(plateau)
-        print('La partie est terminée, bien joué à vous deux !\n\n')
+        print('La partie est terminée, bien joué à vous deux !')
         winner = Fonctions_de_base.Win_Lose(plateau, ia, humain)
         if(winner == ia):            
             win_IA += 1
@@ -274,6 +251,7 @@ if __name__ == '__main__':
         else:
             total_Parties += 1
             print("EGALITE entre IA et Humain à la partie ", total_Parties)
+        print("\n")
 
         #Si une IA surpasse l'autre de 10 victoires
         if(abs(win_IA - win_Humain) >= 10):
